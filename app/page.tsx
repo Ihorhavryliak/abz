1d163dbd2f81dcd94825bda857633fd138028353
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2024-03-07 21:09:51 +0200

    added request token

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import Header from './components/Header/Header'
 import SectionForm from './components/Section/SectionForm/SectionForm'
 import SectionInform from './components/Section/SectionInform/SectionInform'
 import SectionUser from './components/Section/SectionUser/SectionUser'
-import { fetchGetUsers } from '@/redux/slice/UsersSlice'
+import { fetchGetToken, fetchGetUsers } from '@/redux/slice/UsersSlice'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@/redux/store'
 
 export default function Home() {
   const dispatch: AppDispatch = useDispatch()
   useEffect(() => {
+    dispatch(fetchGetToken())
     dispatch(fetchGetUsers())
   }, [dispatch])
 
