9338fb2862a63d85d6c5406b62c04ebfe953ae42
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2024-03-09 21:01:10 +0200

    fixed input

--- components/Input/InputGeneral.tsx
+++ components/Input/InputGeneral.tsx
@@ -1,8 +1,6 @@
 import classNames from '@/utils/classNames'
-import { ChangeEvent, useEffect, useState } from 'react'
 import { UseFormRegister } from 'react-hook-form'
 import { DataInputType, ErrorUserFormType, inputFieldType } from '../Section/SectionForm/hooks/useFormData'
-import useChangePositionLabel from './useChangePositionLabel'
 
 type InputGeneralType = {
   item: DataInputType | { [key: string]: string }
@@ -28,20 +26,18 @@ type UserInformationNameType = keyof inputFieldType
 type UserInformationNameErrorType = keyof ErrorUserFormType
 
 const InputGeneral = ({ errors, label, type = 'text', phone, register, validation, item, value }: InputGeneralType) => {
-  const { handleFocus, isFocus } = useChangePositionLabel()
   const isError = errors && errors[item?.name as UserInformationNameErrorType]
 
   return (
     <div>
-      <div className="relative font-nunito input-component mb-5 empty">
+      <div className="relative font-nunito input-component mb-5 empty group">
         <input
-          onFocus={handleFocus}
           type={type}
           id="floating_outlined"
           className={classNames(
-            isError ? 'border-custom-red-100 border-2' : 'border-custom-gray-200 border',
+            isError ? 'border-custom-red-100 border-2' : 'border-custom-gray-200 border ',
             value ? 'text-custom-black-100' : 'text-custom-gray-300',
-            'w-full py-3.5 ps-4 rounded focus:outline-none peer [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+            'w-full py-3.5 ps-4 rounded focus:outline-none peer [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none group'
           )}
           {...register(item.name as UserInformationNameType, validation)}
           onKeyDown={(e) => {
@@ -54,15 +50,14 @@ const InputGeneral = ({ errors, label, type = 'text', phone, register, validatio
           htmlFor="floating_outlined"
           className={classNames(
             isError ? 'text-custom-red-100' : 'text-custom-gray-300',
-            isFocus || value ? 'text-xs -top-2 transform' : 'text-base top-4 transform',
-            'absolute left-2 transition-all bg-white px-1'
+            !value ? 'top-4 text-base' : '-top-2 text-sm',
+            'absolute left-2 transition-all group-focus:bg-gray-100 bg-white px-1 group-focus-within:-top-2 group-focus-within:text-sm'
           )}
         >
           {label && label}
         </label>
       </div>
       {phone && <div className="text-custom-gray-300 text-xs leading-[117%] ms-4">{phone}</div>}
-
       {isError && (
         <div className="text-custom-red-100 text-xs leading-[117%] ms-4 mt-1">
           {errors[item?.name as UserInformationNameErrorType]?.message}
