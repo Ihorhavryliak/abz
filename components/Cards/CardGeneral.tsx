9a6db854aed291258d7ffe42d7868848f66c346d
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2024-03-10 15:31:59 +0200

    added new styles to card

--- components/Cards/CardGeneral.tsx
+++ components/Cards/CardGeneral.tsx
@@ -5,7 +5,7 @@ type CardGeneralType = {
 }
 const CardGeneral = ({ user }: CardGeneralType) => {
   return (
-    <div className="p-5 font-nunito max-w-[calc(33.333%-19.98px)] max-1024:max-w-[calc(33.333%-19.32px)] max-768:max-w-[calc(50%-8px)] w-full h-[254px] bg-white rounded-[10px] max-360:max-w-full">
+    <div className="p-5 font-nunito max-w-[calc(33.333%-19.98px)] max-1024:max-w-[calc(33.333%-18.32px)px)] max-768:max-w-[calc(50%-8px)] w-full h-[254px] bg-white rounded-[10px] max-360:max-w-full">
       {user?.photo && (
         <div className="flex justify-center">
           <Image width={70} height={70} alt="image" className='rounded-full' src={user?.photo} />
